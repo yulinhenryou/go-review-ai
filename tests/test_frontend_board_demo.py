f97de2739c784a906ff4839f1bc50b1b870e92f5
“Yulin <kylinou@KylindeMacBook-Air.local>
2026-03-10 04:55:00 +1100

    Add chart-click jump interaction for review

--- tests/test_frontend_board_demo.py
+++ tests/test_frontend_board_demo.py
@@ -20,3 +20,8 @@ def test_frontend_board_demo_contains_review_jump_controls() -> None:
     assert 'data-candidate-rank=\\"' in html
     assert "function trySelectCandidateAtPoint(point)" in html
     assert '点击棋盘上的 1 / 2 / 3 候选标记' in html
+    assert "function buildTimelineReviewTarget(data, moveNumber)" in html
+    assert "function activateTimelineMove(moveNumber)" in html
+    assert 'data-chart-move=\\"' in html
+    assert '当前选中第' in html
+    assert '点击图上的点，可直接跳到对应手数' in html
